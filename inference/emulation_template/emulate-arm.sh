cfca8a9fa8971c7a170562a75183cabd6b181e56
4cce8er <acceber5843@hotmail.com>
2025-06-01 13:09:19 +0200

    Add qemu plugin for energy model data
    See Nikov et al: Accurate Energy Modelling on the Cortex-M0 Processor for Profiling and Static Analysis

--- inference/emulation_template/emulate-arm.sh
+++ inference/emulation_template/emulate-arm.sh
@@ -3,6 +3,8 @@ qemu-arm \
 	-g 3333 \
 	-one-insn-per-tb \
 	-plugin ./qemu-bin/contrib/plugins/libmemcnt.so \
+	-plugin ./qemu-bin/contrib/plugins/libpcchangecny.so \
+	-plugin ./qemu-bin/contrib/plugins/libmulcnt.so \
 	-plugin ./qemu-bin/tests/plugin/libinsn.so,inline=true \
 	-d plugin \
 	-D dump-$1.txt \
